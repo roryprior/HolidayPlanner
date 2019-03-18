6a9f4f273d4c4be0edf711297e9ceddb9e0e34a0
Rory Prior <rory@thinkmac.co.uk>
2019-03-18 11:09:38 +0000

    Added some stuff

---
+++ Jet2Test/Model/Excursion.swift
@@ -0,0 +1,29 @@
+//
+//  Excursion.swift
+//  Jet2Test
+//
+//  Represents an excursion
+//
+//  Created by Rory Prior on 15/03/2019.
+//  Copyright © 2019 ThinkMac Software. All rights reserved.
+//
+
+import Foundation
+
+struct Excursion {
+  
+  var destinationName: String
+  var name : String
+  var description : String
+  var price : Double // in pence
+  var image : String
+  
+  var debugDescription: String {
+    return "Excursion: \(name) description: \(description) price: £\(String(format: "%.2f", price / 100)) image: \(image)"
+  }
+  
+  // simple way of generating a unique ID for each excursion
+  func id() -> String {
+    return "\(destinationName)-\(name)"
+  }
+}
